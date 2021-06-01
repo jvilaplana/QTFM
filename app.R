3905e2d6490f782ea726994312304c1f2f112f31
Jordi Vilaplana <jordi.vilaplana@udl.cat>
2021-06-01 20:15:33 +0200

    Updated figure and captions.

--- app.R
+++ app.R
@@ -15,18 +15,19 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             #img(src="fog-model.png", ),
-            numericInput(inputId = "c", label = "# Jobs (Customers)", value = 100, min = 0, max = 500, step = 10),
-            numericInput(inputId = "mu_es", label = "Service rate (ES)", value = 9/10, min = 0.1, max = 1, step = 0.01),
-            numericInput(inputId = "mu_ds", label = "Service rate (DS)", value = 4/10, min = 0.1, max = 1, step = 0.01),
-            numericInput(inputId = "mu_ps", label = "Service rate (PS)", value = 4/10, min = 0.1, max = 1, step = 0.01),
-            numericInput(inputId = "mu_os", label = "Service rate (OS)", value = 4/10, min = 0.1, max = 1, step = 0.01),
-            numericInput(inputId = "mu_fs", label = "Service rate (FS)", value = 4/10, min = 0.1, max = 1, step = 0.01),
-            numericInput(inputId = "mu_cs", label = "Service rate (CS)", value = 4/10, min = 0.1, max = 1, step = 0.01),
-            numericInput(inputId = "m_ps", label = "# Processing Servers", value = 10, min = 1, max = 500, step = 1),
-            numericInput(inputId = "m_fs", label = "# Fog Servers", value = 10, min = 1, max = 500, step = 1),
-            sliderInput(inputId = "d", label = "Database access probability", min = 0, max = 1, step = 0.1, value = 0.5),
-            sliderInput(inputId = "t", label = "Output server probability", value = 0.5, min = 0, max = 1, step = 0.1),
-            sliderInput(inputId = "k", label = "Fog server exit probability", value = 0.5, min = 0, max = 1, step = 0.1),
+            numericInput(inputId = "c", label = HTML("<i>J</i>: # Jobs (Customers)"), value = 100, min = 0, max = 500, step = 10),
+            numericInput(inputId = "mu_es", label = HTML("&#181;<sup>E</sup>: Service rate (ES)"), value = 9/10, min = 0.1, max = 1, step = 0.01),
+            numericInput(inputId = "mu_ds", label = HTML("&#181;<sup>D</sup>: Service rate (DS)"), value = 4/10, min = 0.1, max = 1, step = 0.01),
+            numericInput(inputId = "mu_ps", label = HTML("&#181;<sup>P</sup>: Service rate (PS)"), value = 4/10, min = 0.1, max = 1, step = 0.01),
+            numericInput(inputId = "mu_os", label = HTML("&#181;<sup>O</sup>: Service rate (OS)"), value = 4/10, min = 0.1, max = 1, step = 0.01),
+            numericInput(inputId = "mu_fs", label = HTML("&#181;<sup>F</sup>: Service rate (FS)"), value = 4/10, min = 0.1, max = 1, step = 0.01),
+            numericInput(inputId = "mu_cs", label = HTML("&#181;<sup>C</sup>: Service rate (CS)"), value = 4/10, min = 0.1, max = 1, step = 0.01),
+            numericInput(inputId = "m_ps", label = HTML("R: # Processing Servers"), value = 10, min = 1, max = 500, step = 1),
+            numericInput(inputId = "m_fs", label = HTML("N: # Fog Servers"), value = 10, min = 1, max = 500, step = 1),
+            numericInput(inputId = "m_cs", label = HTML("M: # Client Servers"), value = 10, min = 1, max = 500, step = 1),
+            sliderInput(inputId = "d", label = HTML("&delta;: Database access probability"), min = 0, max = 1, step = 0.1, value = 0.5),
+            sliderInput(inputId = "t", label = HTML("&tau;: Output server probability"), value = 0.5, min = 0, max = 1, step = 0.1),
+            sliderInput(inputId = "k", label = HTML("&kappa;: Fog server exit probability"), value = 0.5, min = 0, max = 1, step = 0.1),
             actionButton("calc", "Calculate"),
         ),
 
@@ -78,7 +79,7 @@ server <- function(input, output) {
         node_ps <- NewInput.MMC(lambda=0, mu=ps_service_rate, c=input$m_ps, n=0)
         node_ds <- NewInput.MM1(lambda=0, mu=ds_service_rate, n=0)
         node_os <- NewInput.MM1(lambda=0, mu=os_service_rate, n=0)
-        node_cs <- NewInput.MMC(lambda=0, mu=cs_service_rate, c=input$c, n=0)
+        node_cs <- NewInput.MMC(lambda=0, mu=cs_service_rate, c=input$m_cs, n=0)
         node_fs <- NewInput.MMC(lambda=0, mu=fs_service_rate, c=input$m_fs, n=0)
         
         # Think time = 0
@@ -139,33 +140,33 @@ server <- function(input, output) {
             image_write(tempfile(fileext='png'), format = 'png')
             
         list(src = tmp_file, contentType = "image/png")
-    })
+    }, deleteFile = TRUE)
 
     output$throughputPlot <- renderPlot({
         m = model()
         df <- data.frame(cust=c(1:input$c), thro=m$througput)
-        ggplot(df, aes(x=cust, y=thro,)) + geom_point() + geom_smooth() + labs(title="Throughput evolution") + xlab("# Customers") + ylab("Throughput")
+        ggplot(df, aes(x=cust, y=thro,)) + geom_point() + geom_smooth() + labs(title="Throughput evolution") + xlab("# Jobs") + ylab("Throughput (jobs/time unit)") + theme(text = element_text(size=18))
         #plot(1:input$c, m$througput, main = "Throughput evolution", ylab = "Throughput", xlab = "# Customers", type = "l", col = "blue")
     })
     
     output$responseTimePlot <- renderPlot({
         m = model()
         df <- data.frame(cust=c(1:input$c), thro=m$mean_time)
-        ggplot(df, aes(x=cust, y=thro,)) + geom_point() + geom_smooth() + labs(title="Mean time evolution") + xlab("# Customers") + ylab("Mean time")
+        ggplot(df, aes(x=cust, y=thro,)) + geom_point() + geom_smooth() + labs(title="Mean time evolution") + xlab("# Jobs") + ylab("Mean time") + theme(text = element_text(size=18))
         #plot(1:input$c, m$mean_time, main = "Mean time evolution", ylab = "Mean Time", xlab = "# Customers", type = "l", col = "blue")
     })
     
     output$meanCustomersPlot <- renderPlot({
         m = model()
         df <- data.frame(cust=c(1:length(m$mean_customers)), thro=m$mean_customers)
-        ggplot(df, aes(x=cust, y=thro)) + geom_bar(stat = "identity") + labs(title="Mean customers evolution") + xlab("# Customers") + ylab("Mean customers")
+        ggplot(df, aes(x=cust, y=thro)) + geom_bar(stat = "identity") + labs(title="Mean customers evolution") + xlab("# Jobs") + ylab("Mean jobs") + theme(text = element_text(size=18))
         #plot(1:input$c, model()$mean_customers, main = "Mean customers evolution", ylab = "Mean Customers", xlab = "# Customers", type = "l", col = "blue")
     })
     
     output$ROkPlot <- renderPlot({
         m = model()
         df <- data.frame(cust=c("ES", "PS", "DS", "OS", "CS", "FS"), thro=m$rok)
-        ggplot(df, aes(x=cust, y=thro)) + geom_bar(stat = "identity") + labs(title="Node Usage") + xlab("Node") + ylab("Usage")
+        ggplot(df, aes(x=cust, y=thro)) + geom_bar(stat = "identity") + labs(title="Node Usage") + xlab("Node") + ylab("Usage") + theme(text = element_text(size=18))
         #plot(1:input$c, model()$mean_customers, main = "Mean customers evolution", ylab = "Mean Customers", xlab = "# Customers", type = "l", col = "blue")
     })
     
